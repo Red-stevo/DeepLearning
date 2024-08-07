ae249d449507defb7ed61303ed74e65ef63d5f42
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-07 22:37:27 +0300

    Commit message >>>>> first sigmoid neuron is doen <<<<<

---
+++ FirstNeuron/SigmoidActivationNeuron/__init__.py
@@ -0,0 +1,22 @@
+import math
+
+
+def sigmoidActivation(add):
+    return 1 / (1 + math.exp(-add))
+
+
+def summation(actions, weights):
+    addition = 0
+
+    for action, weight in zip(actions, weights):
+        addition += action * weight
+
+    return sigmoidActivation(addition)
+
+
+if __name__ == "__main__":
+    # problem space.
+    actions = [.4, .3, .3]
+    weights = [.43, .65, .15]
+
+    print(summation(actions, weights))
