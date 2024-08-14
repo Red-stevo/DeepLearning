1ac9110a5b0d402e96fa6636923b30d61e5840e1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-14 22:44:00 +0300

    Commit message >>>>> working on the multi layered perceptron <<<<<

--- Perceptrons/Perceptrons/__init__.py
+++ Perceptrons/Perceptrons/__init__.py
@@ -1,2 +1,20 @@
-
+import
 # Neuro network, Perceptron neuro network.
+
+class MLP:
+
+    def __init__(self, input_neurons=3, hidden_layers=None, output_neurons=1):
+        if hidden_layers is None:
+            hidden_layers = [3, 5, 2]
+
+        self.input_neurons = input_neurons
+        self.hidden_layers = hidden_layers
+        self.output_neurons = output_neurons
+
+        layers = [self.input_neurons] + self.hidden_layers + [self.output_neurons]
+
+
+
+
+if __name__ == "__main__":
+    MLP()
