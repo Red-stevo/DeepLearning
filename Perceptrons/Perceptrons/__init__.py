074157bb3cebefbd2d85826aff1a5e65c8112366
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-14 23:10:23 +0300

    Commit message >>>>> working on the dependacies. <<<<<

--- Perceptrons/Perceptrons/__init__.py
+++ Perceptrons/Perceptrons/__init__.py
@@ -1,9 +1,12 @@
-import
+import numpy
+
+
 # Neuro network, Perceptron neuro network.
 
 class MLP:
 
     def __init__(self, input_neurons=3, hidden_layers=None, output_neurons=1):
+        # initialize the default array of nodes in the hidden layers.
         if hidden_layers is None:
             hidden_layers = [3, 5, 2]
 
@@ -12,8 +15,6 @@ class MLP:
         self.output_neurons = output_neurons
 
         layers = [self.input_neurons] + self.hidden_layers + [self.output_neurons]
-
-
 
 
 if __name__ == "__main__":
