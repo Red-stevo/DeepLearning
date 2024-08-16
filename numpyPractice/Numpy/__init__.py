24f38121f59e04b51053e7114cb2b88d7b8e945b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-16 21:39:12 +0300

    Commit message >>>>> worked on the dot product. <<<<<

--- numpyPractice/Numpy/__init__.py
+++ numpyPractice/Numpy/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # create an array using the array method.
 def create_array():
-    # one dimesional array
+    # one dimensional array
     arr = np.array([1, 2, 4, 4, 8, 9, 0])
 
     print(f"One dimensional array {arr}")
@@ -13,5 +13,13 @@ def create_array():
     print(f"two dimensional array {arr}")
 
 
+def dot_product():
+    arr1 = np.random.randn(3, 4)
+    arr2 = np.random.randn(4, 2)
+
+    print(f"Dot product {np.dot(arr1, arr2)}")
+
+
 if __name__ == "__main__":
-    create_array()
+    # create_array()
+    dot_product()
